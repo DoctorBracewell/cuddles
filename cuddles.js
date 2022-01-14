10141a8d7b52fff1d9f46a0b81387b3d8ea7b58a
DrBracewell <doctorbracewell@gmail.com>
2022-01-14 20:21:03 +0000

    Small Improvements

--- cuddles.js
+++ cuddles.js
@@ -1,87 +1,71 @@
 const hugs = [
-    "⊂((・▽・))⊃",
-    "＼(^o^)／",
-    "d=(´▽｀)=b",
-    "⊂(◉‿◉)つ",
-    "⊂（♡⌂♡）⊃",
-    "⊂( ◜◒◝ )⊃",
-    "(づ｡◕‿‿◕｡)づ",
-    "(づ￣ ³￣)づ",
-    "(っ˘̩╭╮˘̩)っ",
-    "(っ´▽｀)っ",
-    "༼ つ ◕o◕ ༽つ",
-    "╏つ” ⊡ 〜 ⊡ ” ╏つ",
-    "(⊃｡•́‿•̀｡)⊃",
-    "(つ▀¯▀)つ",
-    "(> ^_^ )>",
-    "⊂(・ヮ・⊂)",
-    "⊂(・▽・⊂)",
-    "ლ(･ω･*ლ)",
-    "ლ(・﹏・ლ)",
-    "───==≡≡ΣΣ((( つºل͜º)つ",
-    "─=≡Σ((( つ ◕o◕ )つ",
-    "ლ(･ω･*ლ)",
-    "ヾ(‘c_’ヽ,,)",
+  "⊂((・▽・))⊃",
+  "＼(^o^)／",
+  "d=(´▽｀)=b",
+  "⊂(◉‿◉)つ",
+  "⊂（♡⌂♡）⊃",
+  "⊂( ◜◒◝ )⊃",
+  "(づ｡◕‿‿◕｡)づ",
+  "(づ￣ ³￣)づ",
+  "(っ˘̩╭╮˘̩)っ",
+  "(っ´▽｀)っ",
+  "༼ つ ◕o◕ ༽つ",
+  "╏つ” ⊡ 〜 ⊡ ” ╏つ",
+  "(⊃｡•́‿•̀｡)⊃",
+  "(つ▀¯▀)つ",
+  "(> ^_^ )>",
+  "⊂(・ヮ・⊂)",
+  "⊂(・▽・⊂)",
+  "ლ(･ω･*ლ)",
+  "ლ(・﹏・ლ)",
+  "───==≡≡ΣΣ((( つºل͜º)つ",
+  "─=≡Σ((( つ ◕o◕ )つ",
+  "ლ(･ω･*ლ)",
+  "ヾ(‘c_’ヽ,,)",
 ];
 
-function newHug() {
-    return hugs[Math.floor(Math.random() * hugs.length)];
-}
+const cuddleText = document.querySelector(".cuddle");
+const hug = hugs[Math.floor(Math.random() * hugs.length)];
 
-function copyToClipboard(str) {
-    const el = document.createElement('textarea');
-    el.value = str;
-    el.setAttribute('readonly', '');
-    el.style.position = 'absolute';
-    el.style.left = '-9999px';
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-};
+document.querySelector("title").innerHTML = hug;
+cuddleText.textContent = hug;
 
-window.onload = () => {
-    const cuddleText = document.querySelector(".cuddle");
+document.querySelector(".label").innerHTML =
+  'cuddles for anyone, anytime, anyplace<br /><br /><span class="text label-small">reload for new hug</span>';
 
-    async function setNewHug() {
-        const hug = newHug(); 
-        document.querySelector("title").innerHTML = hug;
-        cuddleText.textContent = hug;
-    }
+// Clipboard Copying
 
-    let timeouts = [];
+const timeouts = [];
 
-    cuddleText.addEventListener("click", () => {
-        // Reset timeouts
-        for (const timeout of timeouts) {
-            clearTimeout(timeout);
-        }
+cuddleText.addEventListener("click", () => {
+  // Reset timeouts
+  for (const timeout of timeouts) {
+    clearTimeout(timeout);
+  }
 
-        // Copy to clipboard
-        copyToClipboard(cuddleText.textContent);
-        
-        // Reset animation
-        document.documentElement.style.setProperty("--copy-transition", "top");
-        document.documentElement.style.setProperty("--copy-top", "30px");
-        document.documentElement.style.setProperty("--copy-visibility", "0");
+  // Copy to clipboard
+  navigator.clipboard.writeText(cuddleText.textContent);
 
-        // trigger animation
-        document.documentElement.style.setProperty("--copy-visibility", "100");
-        document.documentElement.style.setProperty("--copy-top", "10px");
+  // Reset animation
+  document.documentElement.style.setProperty("--copy-transition", "top");
+  document.documentElement.style.setProperty("--copy-top", "30px");
+  document.documentElement.style.setProperty("--copy-visibility", "0");
 
-        // Set timeouts
-        timeouts.push(setTimeout(() => {
-            document.documentElement.style.setProperty("--copy-transition", "all");
-            document.documentElement.style.setProperty("--copy-visibility", "0");
+  // trigger animation
+  document.documentElement.style.setProperty("--copy-visibility", "100");
+  document.documentElement.style.setProperty("--copy-top", "10px");
 
-            timeouts.push(setTimeout(() => {
-                document.documentElement.style.setProperty("--copy-top", "30px");
-            }, 500));
-        }, 2000))
-    })
+  // Set timeouts
+  timeouts.push(
+    setTimeout(() => {
+      document.documentElement.style.setProperty("--copy-transition", "all");
+      document.documentElement.style.setProperty("--copy-visibility", "0");
 
-    setNewHug().then(() => {
-        document.querySelector(".label").innerHTML =
-            'cuddles for anyone, anytime, anyplace<br /><br /><span class="text label-small">reload for new hug</span>';
-    });
-};
+      timeouts.push(
+        setTimeout(() => {
+          document.documentElement.style.setProperty("--copy-top", "30px");
+        }, 500)
+      );
+    }, 2000)
+  );
+});
