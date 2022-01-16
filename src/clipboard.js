663fb4ef80cb4d1f5874ad0090d42d4e2c95bc6f
DrBracewell <doctorbracewell@gmail.com>
2022-01-16 13:23:49 +0000

    Moved some files around

--- src/clipboard.js
+++ src/clipboard.js
@@ -1,40 +1,4 @@
-const hugs = [
-  "⊂((・▽・))⊃",
-  "＼(^o^)／",
-  "d=(´▽｀)=b",
-  "⊂(◉‿◉)つ",
-  "⊂（♡⌂♡）⊃",
-  "⊂( ◜◒◝ )⊃",
-  "(づ｡◕‿‿◕｡)づ",
-  "(づ￣ ³￣)づ",
-  "(っ˘̩╭╮˘̩)っ",
-  "(っ´▽｀)っ",
-  "༼ つ ◕o◕ ༽つ",
-  "╏つ” ⊡ 〜 ⊡ ” ╏つ",
-  "(⊃｡•́‿•̀｡)⊃",
-  "(つ▀¯▀)つ",
-  "(> ^_^ )>",
-  "⊂(・ヮ・⊂)",
-  "⊂(・▽・⊂)",
-  "ლ(･ω･*ლ)",
-  "ლ(・﹏・ლ)",
-  "───==≡≡ΣΣ((( つºل͜º)つ",
-  "─=≡Σ((( つ ◕o◕ )つ",
-  "ლ(･ω･*ლ)",
-  "ヾ(‘c_’ヽ,,)",
-];
-
-const cuddleText = document.querySelector(".cuddle");
-const hug = hugs[Math.floor(Math.random() * hugs.length)];
-
-document.querySelector("title").innerHTML = hug;
-cuddleText.textContent = hug;
-
-document.querySelector(".label").innerHTML =
-  'cuddles for anyone, anytime, anyplace<br /><br /><span class="text label-small">reload for new hug</span>';
-
 // Clipboard Copying
-
 const timeouts = [];
 
 cuddleText.addEventListener("click", () => {
